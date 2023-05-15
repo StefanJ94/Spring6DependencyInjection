1051806008706bdb3c05128ccc9f4772fcfaca0e
stefanJ94 <jovanovic.stefan94@yahoo.com>
2023-05-15 19:18:46 +0200

    Dependencies Without Injection

--- src/main/java/guru/springframework/spring6dependencyinjection/controllers/MyController.java
+++ src/main/java/guru/springframework/spring6dependencyinjection/controllers/MyController.java
@@ -1,14 +1,22 @@
 package guru.springframework.spring6dependencyinjection.controllers;
 
+import guru.springframework.spring6dependencyinjection.services.GreetingService;
+import guru.springframework.spring6dependencyinjection.services.GreetingServiceImpl;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private final GreetingService greetingService;
+
+    public MyController() {
+        this.greetingService = new GreetingServiceImpl();
+    }
+
     public String sayHello() {
 
         System.out.printf("I'm in the controller");
 
-        return "Hello everyone!";
+        return greetingService.sayGreeting();
     }
 }
