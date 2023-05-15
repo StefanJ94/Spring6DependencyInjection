7d753b668ecfefc13b85116dce52433c9b75e3eb
stefanJ94 <jovanovic.stefan94@yahoo.com>
2023-05-15 21:33:31 +0200

    Default Profile

--- src/main/java/guru/springframework/spring6dependencyinjection/services/i18n/SpanishGreetingService.java
+++ src/main/java/guru/springframework/spring6dependencyinjection/services/i18n/SpanishGreetingService.java
@@ -4,7 +4,7 @@ import guru.springframework.spring6dependencyinjection.services.GreetingService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES","default"})
 @Service("i18NService")
 public class SpanishGreetingService implements GreetingService {
     @Override
