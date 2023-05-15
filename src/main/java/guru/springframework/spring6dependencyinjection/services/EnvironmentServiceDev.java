1ebf753fc34f8681b3d85a96a1c43a5f09a2c25d
stefanJ94 <jovanovic.stefan94@yahoo.com>
2023-05-15 21:57:32 +0200

    Dependency Injection Assignment

---
+++ src/main/java/guru/springframework/spring6dependencyinjection/services/EnvironmentServiceDev.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6dependencyinjection.services;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+@Profile({"dev","default"})
+@Service
+public class EnvironmentServiceDev implements EnvironmentService {
+    @Override
+    public String getEnv() {
+        return "dev";
+    }
+}
